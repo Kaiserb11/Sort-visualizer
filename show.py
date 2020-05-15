5e09fad172c6796319e53c6c342f3291b5dd3333
Krishanu Bhattachajee <47850005+Kaiserb11@users.noreply.github.com>
2020-05-16 02:45:31 +0530

    combined the codes

--- show.py
+++ show.py
@@ -1,37 +1,44 @@
 import pygame
 import random
-import selection_sort
+import sort_algo
 
 pygame.init()
 
 width, high, x, y, rect_high, length, p = 800, 500, 5, 5, 10, 53, 0
 win = pygame.display.set_mode((width, high))
-pygame.display.set_caption("SELECTION SORT")
+pygame.display.set_caption("SORT VISUALIZER")
 val = []
 
 for i in range(0, length):
-    val.append(random.randint(0, high - 10))\
+    val.append(random.randint(0, high - 10))
 
+print("what sorting technique you wanna run \n1. selection \n2. bubble")
+choice = int(input())
 
 running = True
 while running:
-    pygame.time.delay(100)
+    pygame.time.delay(50)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
     win.fill((0, 0, 0))
-    val = selection_sort.selection(val, p)
+
+    if choice == 1:
+        val = sort_algo.selection(val, p)
+    else:
+        val = sort_algo.bubble(val, p)
+
     for j in val:
         pygame.draw.rect(win, (255, 255, 255), (x, y, rect_high, j))
         pygame.display.update()
         x = x + 15
-    
-	p = p + 1
-    if p == length - 1:
+
+    p = p+1
+    if p == len(val)-1:
         p = 0
-    
-	x = 5
+
+    x = 5
 
 
 
--- sort_algo.py
+++ sort_algo.py
@@ -1,3 +1,12 @@
+def bubble(val, p):
+    i = p
+    if val[i] > val[i+1]:
+        temp = val[i]
+        val[i] = val[i+1]
+        val[i+1] = temp
+    return val
+
+
 def selection(val,i):
     min_idx = i
     for j in range(i+1, len(val)):
@@ -5,15 +14,6 @@ def selection(val,i):
             min_idx = j
     val[i], val[min_idx] = val[min_idx], val[i]
     return val
-
-
-
-
-
-
-
-
-
 
 
 
